657edf93f972a6fd46f5241bee46fe3e411cc0e8
Arundhati Bandopadhyaya <38293989+Arundhati1234@users.noreply.github.com>
2022-03-27 08:51:19 +0530

    Create insert.java

---
+++ Arrays/insert.java
@@ -0,0 +1,26 @@
+// To insert an element at a given location in the array
+// Time complexity: O(n)
+
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter length of array: ");
+    int len = in.nextInt();  
+    int arr[] = new int[len+1];
+    System.out.println("Enter items in array: ");
+    for(int i=0; i<len;i++)
+      arr[i] = in.nextInt();
+    System.out.print("Enter index for new element: ");
+    int ind = in.nextInt();
+    System.out.print("Enter element to be pushed: ");
+    int el = in.nextInt();
+    for(int i=len-1;i>=ind;i--)
+      arr[i+1] = arr[i];
+    arr[ind] = el;
+    System.out.println("Final array:");
+    for(int i=0;i<len+1;i++)
+      System.out.print(arr[i]+" ");
+    in.close();
+  }
+}
